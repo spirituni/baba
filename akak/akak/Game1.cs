bcde76f5618335d544c9eee2bd0939dbd13173e2
Judy Chen <jubnaut@gmail.com>
2021-04-21 16:52:39 -0700

    draw grid

--- akak/akak/Game1.cs
+++ akak/akak/Game1.cs
@@ -11,6 +11,12 @@ namespace akak
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        private Texture2D _aki;
+        private Texture2D _blue;
+        private Texture2D _pink;
+        private Texture2D _yellow;
+        private Texture2D _white;
+
         private const string fileName = "test.txt";
         private List<string> fileData = new List<string>();
 
@@ -33,6 +39,12 @@ namespace akak
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _aki = this.Content.Load<Texture2D>("aki");
+            _blue = this.Content.Load<Texture2D>("blue");
+            _pink = this.Content.Load<Texture2D>("pink");
+            _yellow = this.Content.Load<Texture2D>("yellow");
+            _yellow = this.Content.Load<Texture2D>("white");
 
             if (File.Exists(fileName))
             {
@@ -74,15 +86,26 @@ namespace akak
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(color: Color.White);
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(_aki, new Rectangle(0, 0, 50, 50), Color.White);
             /*
-            for (int i = 0; i < w; ++i)
+                        _spriteBatch.Draw(_aki, new Rectangle(0, 0, 50, 50), Color.White);
+                        _spriteBatch.Draw(_blue, new Rectangle(0, 0, 50, 50), Color.White);
+                        _spriteBatch.Draw(_pink, new Rectangle(0, 0, 50, 50), Color.White);
+                        _spriteBatch.Draw(_yellow, new Rectangle(0, 0, 50, 50), Color.White);
+            */
+
+            for (int i = 0; i < _w; ++i)
             {
-                for (int j = 0; j < h; ++j)
+                for (int j = 0; j < _h; ++j)
                 {
-                    _spriteBatch.Draw(tileTexture, new Rectangle(i * tileSize, j * tileSize, tileSize, tileSize), color);
+                    _spriteBatch.Draw(_blue, new Rectangle(i*50, j*50, 50, 50), Color.White);
                 }
             }
-            */
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
